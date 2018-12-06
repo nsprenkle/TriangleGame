b782dc5283362cc6b7cea9262fa19ff0aaaef75e
Claudia Hill <hillc255@yahoo.com>
2018-12-05 22:54:39 -0500

    Refactored code creating functions

--- tri.js
+++ tri.js
@@ -1,16 +1,55 @@
 /* Triangle Game Javascript */
 
-var circleColor = "green";
+// variables
+var circleColorOne = "green";
+var circleArrayGreen = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+var circleArrayBlack = [];
 var resetButton = document.querySelector("#reset");
 var circleOne = document.querySelector("#one");
+var allCircles = document.querySelectorAll(".circle");
 
-
-// begin game
-resetButton.addEventListener("click", function(){
-     alert("Game started");
-
-     if (circleColor === "green"){
-    var col = circleOne.style.backgroundColor = "black";
-    console.log("circleOne color = " + col);
+  //start - reset toggle button
+  resetButton.addEventListener("click", function(){
+     if (circleColorOne === "green"){
+          startGame();  
+    }
+     else{  
+          resetCircles();
      }
-});
+  }); 
+  
+  //function to start game
+  function startGame(){
+          circleOne.style.backgroundColor = "black";
+          circleColorOne = "black";
+          //remove circle 1 from the active array
+          delete circleArrayGreen[0];
+          //add circle 1 to inactive array
+          circleArrayBlack.push(1);
+   }
+
+  //function to reset circle colors
+  function resetCircles(){
+       //reset all circles color to green
+       for(i=0; i < allCircles.length; i++){
+          allCircles[i].style.backgroundColor = "green";
+          }
+          
+       //reset array contents to start
+        circleArrayGreen = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+        circleArrayBlack = [];
+        circleColorOne = "green";
+  }
+
+  // function to change circle color
+  function selectCircle(elem){
+    //onclick only if game is started
+    if( circleColorOne === "black"){
+        var x = elem.id
+          if (document.getElementById(x).style.backgroundColor === 'green') {
+           document.getElementById(x).style.backgroundColor = 'white';
+           } else {
+           document.getElementById(x).style.backgroundColor = 'green';
+       }
+    }
+}
