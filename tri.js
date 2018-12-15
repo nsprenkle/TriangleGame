853168d98912f32da6204491ceefe4448365f63b
Nathan Sprenkle <nathan.sprenkle@gmail.com>
2018-12-14 23:24:42 -0500

    Changes code to use game class

--- tri.js
+++ tri.js
@@ -1,61 +1,65 @@
-/* Triangle Game Javascript */
+class Game {
+  constructor () {
+    this.reset()
+  }
 
-// variables
-let gameStarted = false;
-var circleArrayGreen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-var circleArrayBlack = [];
-var resetButton = document.querySelector("#reset");
-var circleOne = document.querySelector("#one");
-var allCircles = document.querySelectorAll(".circle");
+  reset () {
+    this.board = [[1], [2, 3], [4, 5, 6], [7, 8, 9, 10], [11, 12, 13, 14, 15]]
+  }
 
-//start - reset toggle button
-resetButton.addEventListener("click", function () {
-   if (!gameStarted) {
-      //function to start game
-      startGame();
-   } else {
-      //function to reset all circles to green
-      resetCircles();
-   }
-});
+  move (startPiece, endPiece) {
+    // Valid move if...
+    let valid = false
+    let jumpedPiece = {}
 
-//function to start game
-function startGame() {
-   circleOne.style.backgroundColor = "black";
-   gameStarted = true;
-   //remove circle 1 from the active array
-   delete circleArrayGreen[0];
-   //add circle 1 to inactive array
-   circleArrayBlack.push(1);
+    const rowDelta = endPiece.row - startPiece.row
+    const columnDelta = endPiece.column - startPiece.column
+
+    console.log(`Row Delta: ${rowDelta}\tColumn Delta: ${columnDelta}`)
+
+    // In the same row but difference of exactly 2
+    valid = valid || (rowDelta === 0 && Math.abs(columnDelta) === 2)
+
+    // 2 rows away with an offset of 0 or 2 columns
+    valid = valid || (Math.abs(rowDelta) === 2 && (columnDelta === 0 || Math.abs(columnDelta) === 2))
+
+    if (!valid) { console.error('Invalid move: bad row/column'); return false }
+
+    // There is a piece in the starting spot
+    valid = valid && (this.board[startPiece.row][startPiece.column] != null)
+    if (!valid) { console.error('Invalid move: no piece at start location'); return false }
+
+    // There is no piece in the ending spot
+    valid = valid && (this.board[endPiece.row][endPiece.column] == null)
+    if (!valid) { console.error('Invalid move: piece at end location'); return false }
+
+    // Find jumped piece
+    if (rowDelta === 0) {
+      jumpedPiece.row = startPiece.row
+      jumpedPiece.column = (columnDelta / 2) + startPiece.column
+    } else {
+      jumpedPiece.row = (rowDelta / 2) + startPiece.row
+      jumpedPiece.column = (columnDelta / 2) + startPiece.column
+    }
+
+    // There must be a piece getting jumped
+    if (!this.remove(jumpedPiece)) { return false }
+
+    return valid
+  }
+
+  /** Removes a piece from the board
+           * @returns whether or not a piece existed in the spot before and could be removed
+           */
+  remove (piece) {
+    if (this.board[piece.row][piece.column] == null) {
+      console.error('Invalid remove: no piece to remove')
+      return false
+    }
+
+    this.board[piece.row][piece.column] = null
+    return true
+  }
 }
 
-//function to reset circle colors
-function resetCircles() {
-   //reset all circles color to green
-   for (i = 0; i < allCircles.length; i++) {
-      allCircles[i].style.backgroundColor = "green";
-   }
-   //reset array contents to start
-   circleArrayGreen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-   circleArrayBlack = [];
-   gameStarted = false;
-}
-
-// function to change circle color to white onclick if game started
-window.onload = function () {
-   var anchors = document.getElementsByClassName("circle");
-   for (var i = 1; i < anchors.length; i++) {
-      var anchor = anchors[i];
-      anchor.onclick = function () {
-         var x = this.id;
-         //onclick only if game is started
-         if (gameStarted) {
-            if (document.getElementById(x).style.backgroundColor === 'green') {
-               document.getElementById(x).style.backgroundColor = 'white';
-            } else if (document.getElementById(x).style.backgroundColor === 'black') {
-               document.getElementById(x).style.backgroundColor = 'green';
-            }
-         }
-      }
-   }
-}
+module.exports = Game
